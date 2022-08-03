938eee4074aede6be78a0c3c6dc1ebace83ed8c1
CCLDArjun <ccldarjun@icloud.com>
2022-08-03 13:32:12 -0700

    both happiness and time spent

--- backend/app.py
+++ backend/app.py
@@ -113,7 +113,12 @@ def graph_data(username):
     buf = BytesIO()
     fig.savefig(buf, format="png")
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return jsonify({"data": data})
+
+    ax.plot([matplotlib.dates.date2num(x), spent])
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    timespent = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return jsonify({"happiness": data, "time_spent": timespent})
 
 @app.route("/signin", methods=['POST'])
 def signin():
